4537546a7e0a0531729622d2f1a1b9b2a240f9f3
Donovan Bergin <kaiserbergin@gmail.com>
2018-05-18 11:08:26 -0500

    added tutorial cube

---
+++ Assets/Node Hacker/Scripts/Levels/IntroCube.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class IntroCube : MonoBehaviour {
+    public List<string> acceptableTags;
+    public VideoPlayer introVideoPlayer;
+    public GameObject interroBangCube;
+    public GameObject introCube;
+
+    private void Awake() {
+        if (acceptableTags == null || acceptableTags.Count == 0) {
+            acceptableTags = new List<string> {
+                "Player",
+                "Left Hand",
+                "Right Hand"
+            };
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if ((acceptableTags.Contains(collision.gameObject.tag) || collision.gameObject.layer == LayerMask.NameToLayer("Projectile"))) {
+            if (interroBangCube.activeInHierarchy) {
+                interroBangCube.SetActive(false);
+                introCube.SetActive(true);
+                introVideoPlayer.Play();
+            } else {
+                if(introVideoPlayer.isPlaying) {
+                    introVideoPlayer.Pause();
+                } else {
+                    introVideoPlayer.Play();
+                }
+            }
+        }
+    }
+}
